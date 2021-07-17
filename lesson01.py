f088a53afa89895f2216ed0d1d5c5df664b55ccb
byungju0624 <byungju0624@gmail.com>
2021-07-17 11:30:45 +0900

    first lesson in Python

---
+++ lesson01.py
@@ -0,0 +1,14 @@
+# foo = input()
+# print(foo)
+
+# a = 10
+# b = a
+# c = b + 10
+# print(c)
+
+# foo = input()
+# print(len(input()))
+# foo = input("문자를 입력해 주세요\n")
+# result = len(foo)
+# print(f"글자수 : {result}글자") #f: 문자열 포맷팅
+
---
+++ practice01.py
@@ -0,0 +1,3 @@
+firstNum = int(input("첫번째 숫자를 입력해주세요"))
+secondNum = int(input("두번째 숫자를 입력해주세요"))
+print(firstNum * secondNum)
